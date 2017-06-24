bd86abb0cc0b570a0f54ead495a701f16ae67ce8
Jonathan Otto <jonathan.otto@gmail.com>
2017-06-24 12:39:22 -0700

    Add code splitting component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Loadable from "react-loadable";
+const HelloWorld = Loadable({
+  loader: () => import("./HelloWorld"),
+  loading: () => <div>Loading...</div>
+});
 
 class App extends Component {
   render() {
@@ -13,6 +19,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <HelloWorld />
       </div>
     );
   }
